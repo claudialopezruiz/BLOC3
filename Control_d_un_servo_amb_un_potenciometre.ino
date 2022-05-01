aa98af3cc533b45a2a8d4d4d7b3dd23cd9b1afae
claudialopezruiz <101014132+claudialopezruiz@users.noreply.github.com>
2022-05-01 18:32:06 +0200

    Add files via upload

---
+++ Control_d_un_servo_amb_un_potenciometre.ino
@@ -0,0 +1,21 @@
+#include <Servo.h>
+
+Servo servo1;  
+
+int valor;  
+
+void setup() {
+  servo1.attach(3);
+  }
+
+void loop() {
+  valor = analogRead(0); 
+  valor = map(valor, 0, 1023, 0, 180);  
+  servo1.write(valor);  
+  delay(15); 
+}
+
+
+
+
+  
